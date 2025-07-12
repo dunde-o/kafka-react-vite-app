892b317f3137a95746f3a1bccafebb43f82f3d09
inkyu.choi <chldlsrb1000@naver.com>
2025-07-12 12:40:53 +0000

    python 환경 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,20 @@ import { VITE_API_URL } from './env/env'
 function App() {
   const [count, setCount] = useState(0)
 
-  const handleClick = async () => {
+  const handleSpringBootApiClick = async () => {
     const res = await fetch(VITE_API_URL + '/demo/message?message=Hello', { method: 'GET' })
     console.log('Response:', res)
+  }
+  const handlePythonApiClick = async () => {
+    const res = await fetch(VITE_API_URL + '/python/message', { 
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: 'Hello from React' })
+    })
+    const data = await res.json()
+    console.log('result:', data)
   }
 
   return (
@@ -27,8 +38,11 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <button onClick={handleClick}>
-          api call
+        <button onClick={handleSpringBootApiClick}>
+          spring boot api call
+        </button>
+        <button onClick={handlePythonApiClick}>
+          python api call
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
